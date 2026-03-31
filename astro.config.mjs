36533b06acb5e422145f9b5ddbc1e2833bcbd20a
shcheran <anastasia.shcherban@gmail.com>
2026-03-31 10:03:23 +0300

    fix: logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,9 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(),
-		starlight({
-      title: 'Compbio Blog',
-      logo: {
-        src: './src/assets/logo.png', // Path to your logo
-      },
-    }),
-	],
+	integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-export const SITE_TITLE = 'Bioinformatics Signals';
+export const SITE_TITLE = 'Compbio & Bioinformatics Blog';
 export const SITE_DESCRIPTION =
   'AI-assisted, manually curated signals across bioinformatics research, tools, and industry.';
